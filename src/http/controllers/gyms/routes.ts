2983f0e5e3140484728c709d0940800efe751ce3
Fernando Soares <fernando.hfs94@gmail.com>
2023-06-22 09:01:52 -0300

    feat(controller): add gyms remaining

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,9 +2,14 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from './create-gym.controller'
+import { search } from './search-gyms.controller'
+import { nearby } from './fetch-nearby-gyms.controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms', create)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 }
