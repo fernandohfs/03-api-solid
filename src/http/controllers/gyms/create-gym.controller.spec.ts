92e69af9a4c8fbf470d43df3f33d74e68d3f429e
Fernando Soares <fernando.hfs94@gmail.com>
2023-06-23 08:36:36 -0300

    refactor(test): create isolate util function

--- src/http/controllers/gyms/create-gym.controller.spec.ts
+++ src/http/controllers/gyms/create-gym.controller.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
 describe('Create Gym (e2e)', () => {
   beforeAll(async () => {
@@ -13,18 +14,7 @@ describe('Create Gym (e2e)', () => {
   })
 
   it('should be able to create gym', async () => {
-    await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456',
-    })
-
-    const authResponse = await request(app.server).post('/sessions').send({
-      email: 'johndoe@example.com',
-      password: '123456',
-    })
-
-    const { token } = authResponse.body
+    const { token } = await createAndAuthenticateUser(app)
 
     const createGymResponse = await request(app.server)
       .post('/gyms')
